8faee8533601c52856628e53c5ebc2072ae9426f
rick-liyue-huang <rick.liyue.huang@gmail.com>
2019-07-22 14:13:56 +1000

    just staryt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: string,
+  link?: string
+}
+
+interface AddFunc {
+  (x: number, y: number): number
+}
+
+interface Dict {
+  [key: string]: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GroupBuy-Angular';
+  menus: TopMenu[] = [
+    {
+      title: 'Hot',
+      link: ''
+    },
+    {
+      title: 'Male',
+      link: ''
+    },
+    {
+      title: 'Mobile',
+      link: ''
+    },
+    {
+      title: 'Hot',
+      link: ''
+    },
+    {
+      title: 'Male',
+      link: ''
+    },
+    {
+      title: 'Mobile',
+      link: ''
+    },
+    {
+      title: 'Hot',
+      link: ''
+    },
+    {
+      title: 'Male',
+      link: ''
+    },
+    {
+      title: 'Mobile',
+      link: ''
+    }
+  ];
+  selectedIndex = -1;
+  constructor() {
+    console.log(this.dict['a']);
+  }
+  add: AddFunc = (x, y) => x + y;
+  dict: Dict = {
+    a: '6',
+    b: '66'
+  }
 }
